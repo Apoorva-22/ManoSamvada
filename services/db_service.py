5a0fd44f2da25ae076561936a2f037cc1b697947
Apoorva-22 <99539940+Apoorva-22@users.noreply.github.com>
2026-05-26 00:43:28 +0530

    Update db_service.py

--- services/db_service.py
+++ services/db_service.py
@@ -1,20 +1,11 @@
 import os
-import certifi
-import mysql.connector
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["SSL_CERT_FILE"] = certifi.where()
 
 def get_db():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        use_pure=True,
-        ssl_disabled=True,
-        charset="utf8mb4",
-        use_unicode=True
+    return psycopg2.connect(
+        os.getenv("DATABASE_URL")
     )
