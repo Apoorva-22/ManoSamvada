0ec62ff4a4f2c46b97b59badb5c34516020d5a65
Apoorva-22 <99539940+Apoorva-22@users.noreply.github.com>
2026-05-25 22:48:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,10 @@ app.register_blueprint(admin_bp)
 app.register_blueprint(analytics_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
